fb75e99806c3f3053b73b3b9cdc9108753e19b98
rilo.pambudi1998@gmail.com <45749606+ekomrp177@users.noreply.github.com>
2020-04-01 09:35:40 +0700

    UI & Multiply

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private var number1 : Int = 0
+    private var number2 : Int = 0
+    private var resultNumb : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMultiplication.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (txtNumber1.text.isEmpty()){
+            txtNumber1.error = "Field Cannot Blank"
+        }
+        else if (txtNumber2.text.isEmpty()){
+            txtNumber2.error = "Field Cannot Blank"
+        }
+        else{
+            number1 = txtNumber1.text.toString().toInt()
+            number2 = txtNumber2.text.toString().toInt()
+            when (v.id) {
+                R.id.btnMultiplication -> {
+                    resultNumb = Multiplication(number1, number2)
+                    txtResult.setText(resultNumb.toString())
+                }
+
+            }
+        }
+    }
+
+    private fun Multiplication(number1: Int, number2: Int) : Int {
+        return number1 * number2
     }
 }
