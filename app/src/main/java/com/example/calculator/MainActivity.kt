6e365a65633c7fcf7b0667e2edb04a594513b552
Bayu Alif Farisqi <48192376+bayualiffarisqi@users.noreply.github.com>
2020-04-01 08:15:15 +0700

    make a aditional methode

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,5 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        btnTambah.setOnClickListener {
+            if (txtNumber1.text.toString().isEmpty() || txtNumber2.text.toString().isEmpty()) {
+                txtNumber1.setError("Semua input harus diisi!")
+            } else {
+                val a = txtNumber1.text.toString().toDouble()
+                val b = txtNumber2.text.toString().toDouble()
+                val c = this.tambah(a, b)
+                txtHasil.setText(c.toString())
+            }
+        }
+    }
+     fun tambah(a: Double, b: Double): Double {
+        return a + b
     }
 }
